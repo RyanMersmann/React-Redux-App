8a9a2a14e7b2d8edeae05a4ae7655456e6d7aee5
RyanMersmann <mersmannryan@gmail.com>
2020-06-24 21:25:10 -0600

    added dad jokes guided project as a guide

--- app/src/index.js
+++ app/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { store } from './App';
+import { createStore, applyMiddleware } from "redux";
+import { appReducer } from "./reducers";
+import { Provider } from "react-redux";
 
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
